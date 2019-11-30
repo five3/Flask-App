1a4ee5199f50a83c43de8af497a195cac6bad962
chenxiaowu <chenxiaowu@autohome.com.cn>
2019-11-30 23:00:04 +0800

    update

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,2 +1,17 @@
+import os
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("cmd", help="start a flask project or app")
+    parser.add_argument("name", help="flask project or app name")
+    return parser.parse_args()
+
+
 def main():
-    pass
+    args = parse_args()
+    if args.cmd == 'startproject':
+        print(f'Will create Flask project with name {args.name}')
+    elif args.cmd == 'startapp':
+        print(f'Will create Flask app with name {args.name}')
