690d159d47c43e5fd31bb0cc8e167ef5e6399306
chenxiaowu <chenxiaowu@autohome.com.cn>
2019-11-30 23:41:45 +0800

    update

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_desc():
 setup( 
     name="Flask_App",
     version="0.1.0",
-    keywords=("Flask", "Falsk App", "Falsk Project"),
+    keywords=["Flask", "Falsk App", "Falsk Project"],
     description="create application template for flask.",
     long_description=get_desc(),
     license="GUN V3",
@@ -25,10 +25,13 @@ setup(
     author_email="five3@163.com",
 
     package_dir={'flask_app': 'flask_app'},         # 指定哪些包的文件被映射到哪个源码包
-    packages=['flask_app'],       # 需要打包的目录。如果多个的话，可以使用find_packages()自动发现
+    packages=find_packages(),       # 需要打包的目录。如果多个的话，可以使用find_packages()自动发现
     include_package_data=True,
+    package_data={'flask_app': ['templates/*']},
     py_modules=[],          # 需要打包的python文件列表
-    data_files=[],          # 打包时需要打包的数据文件
+    data_files=[
+        'flask_app/templates/app_tmpl/__init__.py',
+    ],          # 打包时需要打包的数据文件
     platforms="any",
     install_requires=[      # 需要安装的依赖包
         'flask>=1.0.2'
@@ -36,7 +39,7 @@ setup(
     scripts=[],             # 安装时需要执行的脚本列表
     entry_points={
         'console_scripts': [    # 配置生成命令行工具及入口
-            'flask = flask_app:main'
+            'flask-app = flask_app:main'
         ]
     },
     classifiers=[           # 程序的所属分类列表
