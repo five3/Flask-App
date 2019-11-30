dbc1ed51a2029659ebe71a29b71e3ffd355b0538
chenxiaowu <chenxiaowu@autohome.com.cn>
2019-11-30 23:12:41 +0800

    update

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import argparse
 
 
@@ -13,5 +14,12 @@ def main():
     args = parse_args()
     if args.cmd == 'startproject':
         print(f'Will create Flask project with name {args.name}')
+        dir_name = os.path.dirname(__file__)
+        source_dir = os.path.join(dir_name, 'templates', 'project_tmpl')
+        dist_dir = os.path.join(os.getcwd(), args.name)
+        print(source_dir)
+        print(dist_dir)
+        shutil.copytree(source_dir, dist_dir)
     elif args.cmd == 'startapp':
         print(f'Will create Flask app with name {args.name}')
+        shutil.copytree('./templates/app_tmpl', os.getcwd())
---
+++ test/startproject.py
@@ -0,0 +1,3 @@
+from flask_app import main
+
+main()
