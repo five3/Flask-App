d8fcb5961358dce158e201a9e29c94bc7b7dca5f
chenxiaowu <chenxiaowu@autohome.com.cn>
2019-12-01 19:37:26 +0800

    update

--- flask_app/templates/project_tmpl/manage.py
+++ flask_app/templates/project_tmpl/manage.py
@@ -1,8 +1,19 @@
-from apps.utils import parse_args, init_env_path
+import os
+import sys
+
+
+def init_env_path(_file_):
+    package_dir = os.path.join(os.path.dirname(_file_), '../')
+    abs_path = os.path.abspath(package_dir)
+    if abs_path not in sys.path:
+        print(f'Add {abs_path} to python path')
+        sys.path.insert(0, abs_path)
+
 
 init_env_path(__file__)
 
-from flask_app.apps import create_app
+from apps.utils import parse_args
+from apps import create_app
 
 __all__ = ['main']
 
@@ -10,9 +21,19 @@ __all__ = ['main']
 def main():
     app = create_app()
     args = parse_args()
-    port = args.port if args.port else 8080
-    app.run('0.0.0.0', port)
-    app.logger.info(f'run server at port {port}')
+
+    if args.cmd.isdigit():
+        cmd = 'runserver'
+        port = int(args.cmd)
+    else:
+        cmd = args.cmd
+        port = args.port if args.port else 8080
+
+    if cmd == 'runserver':
+        app.run('0.0.0.0', port)
+        app.logger.info(f'run server at port {port}')
+    else:
+        print('run command error')
 
 
 if __name__ == '__main__':
